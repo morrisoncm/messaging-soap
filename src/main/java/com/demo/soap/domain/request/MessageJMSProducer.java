62e59b0107c5fed38e1e05f22da554ea877ee268
cmorrison <cmorrison@tsys.com>
2022-08-23 01:26:02 -0400

    Training

--- src/main/java/com/demo/soap/domain/request/MessageJMSProducer.java
+++ src/main/java/com/demo/soap/domain/request/MessageJMSProducer.java
@@ -1,8 +1,18 @@
 package com.demo.soap.domain.request;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageJMSProducer {
 
+  @Autowired
+  private JmsTemplate jmsTemplate;
+
+  public String sendMessage(String message) {
+    jmsTemplate.convertAndSend("DEV.QUEUE.1", message);
+    return "message sent";
+  }
+ 
 }
--- src/main/java/com/demo/soap/domain/response/MessageJMSConsumer.java
+++ src/main/java/com/demo/soap/domain/response/MessageJMSConsumer.java
@@ -1,8 +1,17 @@
 package com.demo.soap.domain.response;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageJMSConsumer {
 
+  @Autowired
+  private JmsTemplate jmsTemplate;
+
+  public String consumeMessage() {
+    return jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString();
+  }
+ 
 }
